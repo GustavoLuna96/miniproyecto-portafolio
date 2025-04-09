8564c5bb623f4eb1e3b271c6c4afaaf0242fd7c5
Gustavo <tavoooo92@hotmail.com>
2025-04-09 13:50:25 -0600

    new functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0D1117",
+        light: "#FFFFFF",
+        highlight: "#E3FF6A",
+        neonGreen: "#A4FF7A",
+        deepBlue: "#1E293B",
+        softGray: "#8892B0",
+      },
+      backgroundImage: {
+        "gradient-main": "linear-gradient(135deg, #1E293B 0%, #0D1117 100%)",
+      },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}gi
